5aeb1ee9528d7ea6c92d306e0d9e7f21286a48b1
Денис Фадеев <d.fadeev@cashoff.ru>
2025-05-13 22:02:48 +0300

    Добавлены методы поиска элементов в базовый класс чтобы скрыть детали взаимодействие с драйвером

--- pages/base.py
+++ pages/base.py
@@ -1,8 +1,24 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+
 class BasePage:
 
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+
+    def find_element(self, locator: tuple[str, str], timeout: int = 10):
+        return WebDriverWait(self.browser, timeout=timeout).until(
+            ec.presence_of_element_located(locator),
+            message=f'Element {locator} not found'
+        )
+
+    def find_elements(self, locator: tuple[str, str], timeout: int = 10):
+        return WebDriverWait(self.browser, timeout=timeout).until(
+            ec.presence_of_all_elements_located(locator),
+            message=f'Elements {locator} not found'
+        )
 
     def open_page(self):
         self.browser.get(self.url)
